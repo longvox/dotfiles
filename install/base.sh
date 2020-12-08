60fae069112f03c01dbccf844cbfdd0d60a4762b
Long Vo <longvox98@gmail.com>
2020-12-09 04:38:31 +0700

    [imlong] - update install package base.sh

--- install/base.sh
+++ install/base.sh
@@ -1,77 +1,50 @@
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
+#!/usr/bin/env bash
+source $(dirname "$(readlink -f "$BASH_SOURCE")")/utils/log.sh
+source $(dirname "$(readlink -f "$BASH_SOURCE")")/utils/install.sh
 
-echo "${green}Update and Upgrade!${reset}"
+info "Update and Upgrade!"
 
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
 
-echo "${green}Installing base app!${reset}"
-sudo apt install -y \
-   software-properties-common \
-   apt-transport-https \
-   ca-certificates \
-   gnupg-agent \
-   build-essential \
-   mesa-common-dev \
-   libglu1-mesa-dev \
-   libxrandr-dev \
-   libssl-dev \
-   libffi-dev \
-   libxml2-dev \
-   libx11-dev \
-   libxslt1-dev \
-   zlib1g-dev \
-   libxi-dev \
-   libxext-dev \
-   libxtst-dev \
-   curl \
-   wget \
-   git \
-   ruby \
-   htop
+info "Installing base app!"
 
-echo "${green}Installing python pip!${reset}"
-sudo apt install -y \
-   python-dev \
-   python3-dev \
-   python3-pip
+tryInstall install software-properties-common
+tryInstall install apt-transport-https
+tryInstall install ca-certificates
+tryInstall install gnupg-agent
+tryInstall install build-essential
+tryInstall install mesa-common-dev
+tryInstall install libglu1-mesa-dev
+tryInstall install libxrandr-dev
+tryInstall install libssl-dev
+tryInstall install libffi-dev
+tryInstall install libxml2-dev
+tryInstall install libx11-dev
+tryInstall install libxslt1-dev
+tryInstall install zlib1g-dev
+tryInstall install libxi-dev
+tryInstall install libxext-dev
+tryInstall install libxtst-dev
+tryInstall install curl
+tryInstall install wget
+tryInstall install git
+tryInstall install ruby
+tryInstall install htop
+tryInstall install python-dev
+tryInstall install python3-dev
+tryInstall install python3-pip
 
-echo "${green}Install docker!${reset}"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+tryInstall install docker-ce
+tryInstall install docker-ce-cli
+tryInstall install containerd.io
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable" -y
+echo "Installing node, npm, nvm, yarn!"
+tryInstall install nodejs
+tryInstall install npm
+tryInstall install yarn
+tryInstall installSh https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-sudo docker run hello-world
-
-echo "${green}Done install docker!${reset}"
-
-echo "${green}Installing node, npm, nvm, yarn!${reset}"
-sudo apt install -y nodejs
-sudo apt install -y npm
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-sudo apt update && sudo apt install yarn
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-echo "${green}Done install node, npm, nvm, yarn!${reset}"
-
-# echo "${green}Installing vim!${reset}"
-# sudo add-apt-repository ppa:jonathonf/vim -y
-# sudo apt install -y vim
-
-echo "${green}Installing neovim!${reset}"
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage
+tryInstall install vim
+tryInstall install neovim
