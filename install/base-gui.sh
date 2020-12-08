cd11e9831b7363079cfcfff331b01c69a1102f81
Long Vo <longvox98@gmail.com>
2020-12-09 04:36:15 +0700

    [imlong] - update log base-gui.sh

--- install/base-gui.sh
+++ install/base-gui.sh
@@ -1,63 +1,30 @@
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
+#!/usr/bin/env bash
+source $(dirname "$(readlink -f "$BASH_SOURCE")")/utils/log.sh
+source $(dirname "$(readlink -f "$BASH_SOURCE")")/utils/install.sh 
 
-if [[ $(whoami) == "administrator" || $(whoami) == "imlong" ]];
+if [[ $(whoami) == "administrator"
+    || $(whoami) == "imlong"
+    || $(whoami) == "longvox" ]];
 then
-    echo "${green}Install GUI application!${reset}"
+    info "Install GUI application!"
 
-    echo "${green}Installing gnome  tweak tool!${reset}"
-    dpkg -s gnome-tweak-tool &> /dev/null || sudo apt install gnome-tweak-tool -y
-
-    echo "${green}Installing Gufw Firewall!${reset}"
-    dpkg -s gufw &> /dev/null || sudo apt install gufw -y
-
-    dpkg -s gdebi-core &> /dev/null || sudo apt install gdebi-core -y
-
-    dpkg -s ubuntu-restricted-extras &> /dev/null || sudo apt-get install -y ubuntu-restricted-extras
-
-    echo "${green}Installing kazam!${reset}"
-    dpkg -s kazam &> /dev/null || sudo apt-get install kazam -y
-
-    echo "${green}Install flameshot!${reset}"
-    dpkg -s flameshot &> /dev/null || sudo apt-get install -y flameshot
-
-    echo "${green}Installing vlc!${reset}"
-    dpkg -s vlc &> /dev/null || sudo apt-get install vlc -y
-
-    echo "${green}Installing skype!${reset}"
-    wget https://repo.skype.com/latest/skypeforlinux-64.deb
-    sudo dpkg -i skypeforlinux-64.deb
-    sudo apt-get install -f
-
-    echo "${green}Installing mailspring!${reset}"
-    sudo snap install mailspring
-
-    echo "${green}Installing google chrome!${reset}"
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo gdebi google-chrome-stable_current_amd64.deb
-
-    echo "${green}Installing firefox${reset}"
-    dpkg -s firefox &> /dev/null || sudo apt install -y firefox
-
-    echo "${green}Installing spotify!${reset}"
-    sudo snap install spotify
-
-    echo "${green}Installing onlyoffice!${reset}"
-    sudo snap install onlyoffice-desktopeditors
-
-    echo "${green}Installing postman!${reset}"
-    sudo snap install postman
-
-    echo "${green}Installing zoom-client!${reset}"
-    sudo snap install zoom-client
-
-    echo "${green}Installing caprine!${reset}"
-    sudo snap install caprine
-
-    echo "${green}Installing alacritty!${reset}"
-    sudo add-apt-repository ppa:mmstick76/alacritty
-    dpkg -s alacritty &> /dev/null || sudo apt install alacritty
+    tryInstall install gnome-tweak-tool
+    tryInstall install gufw
+    tryInstall install gdebi-core
+    tryInstall install ubuntu-restricted-extras
+    tryInstall install kazam
+    tryInstall install flameshot
+    tryInstall install vlc
+    tryInstall installDeb https://repo.skype.com/latest/skypeforlinux-64.deb
+    tryInstall installDeb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    tryInstall install firefox
+    tryInstall installSnap spotify
+    tryInstall installSnap mailspring
+    tryInstall installSnap onlyoffice-desktopeditors
+    tryInstall installSnap postman
+    tryInstall installSnap zoom-client
+    tryInstall installSnap caprine
+    tryInstall install alacritty
 else
-    echo "${red}Not available GUI!${reset}"
+    error "Not available GUI!"
 fi
