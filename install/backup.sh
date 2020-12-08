2f027f1d074099537c4b21255b0e4fd4e7e29cb3
Long Vo <longvox98@gmail.com>
2020-12-09 04:34:36 +0700

    [imlong] - update log backup.sh

--- install/backup.sh
+++ install/backup.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
+source $(dirname "$(readlink -f "$BASH_SOURCE")")/utils/log.sh
 
 # Backup files that are provided by the dotfiles into a ~/dotfiles-backup directory
-
 DOTFILES=$HOME/.dotfiles
 BACKUP_DIR=$HOME/dotfiles-backup
 
-# colors output
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
-
-set -e # Exit immediately if a command exits with a non-zero status.
-
-echo "${green}Creating backup directory at $BACKUP_DIR${reset}"
+info "Creating backup directory at $BACKUP_DIR"
 mkdir -p "$BACKUP_DIR"
 
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
@@ -21,18 +14,18 @@ for file in $linkables; do
     filename=".$( basename "$file" '.symlink' )"
     target="$HOME/$filename"
     if [ -f "$target" ]; then
-        echo "${green}backing up $filename${reset}"
+        info "backing up $filename"
         cp "$target" "$BACKUP_DIR"
     else
-        echo "${red}$filename does not exist at this location or is a symlink${reset}"
+        error "$filename does not exist at this location or is a symlink"
     fi
 done
 
 for filename in "$HOME/.config/nvim" "$HOME/.config/alacritty" "$HOME/.vim" "$HOME/.vimrc"; do
     if [[ ! -L "$filename" && -d "$filename" ]]; then
-        echo "${green}backing up $filename${reset}"
+        info "backing up $filename"
         cp -rf "$filename" "$BACKUP_DIR"
     else
-        echo "${red}$filename does not exist at this location or is a symlink${reset}"
+        error "$filename does not exist at this location or is a symlink"
     fi
 done
