df8256c7bb9dd954aeec2fdd73b82a8276f5a885
Long Vo <longvox98@gmail.com>
2020-12-09 04:34:14 +0700

    [imlong] - update log install.sh

--- install.sh
+++ install.sh
@@ -1,80 +1,73 @@
 #!/usr/bin/env bash
-
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
-
-command_exists() {
-    type "$1" > /dev/null 2>&1
-}
-
-set -eE
+source $(dirname "$(readlink -f "$BASH_SOURCE")")/install/utils/log.sh
+source $(dirname "$(readlink -f "$BASH_SOURCE")")/install/utils/install.sh
 
 # set policies executable
-echo "${green}set policies executable${reset}"
+info "set policies executable"
 chmod +x install/backup.sh
 chmod +x install/link.sh
+chmod +x install/base-repository.sh
 chmod +x install/base.sh
 chmod +x install/base-gui.sh
 chmod +x install/base-nvim.sh
 chmod +x install/base-zsh.sh
 chmod +x install/git.sh
 
-echo "${green}Backups dotfiles.${reset}"
+info "Backups dotfiles."
 . install/backup.sh
 
-echo "${green}Link dotfiles.${reset}"
+info "Link dotfiles."
 . install/link.sh
 
-echo "${green}Installing dotfiles.${reset}"
+info "Installing dotfiles."
 . install/base.sh
 . install/git.sh
 . install/base-gui.sh
 . install/base-nvim.sh
 . install/base-zsh.sh
 
-echo "${green}After install dotfiles.${reset}"
+info "After install dotfiles."
 . install/after-install.sh
 
 # only perform macOS-specific install
 # after the install, install neovim python libraries
-echo -e "${green}\\n\\nRunning Neovim Python install${reset}"
-echo "${green}==============================${reset}"
-pip3 install pynvim
+info "Running Neovim Python install"
+info "${green}==============================${reset}"
+tryInstall installPip pynvim
 
 # Change the default shell to zsh
 zsh_path="$( command -v zsh )"
 if ! grep "$zsh_path" /etc/shells; then
-    echo "${green}adding $zsh_path to /etc/shells${reset}"
-    echo "${green}$zsh_path${reset}" | sudo tee -a /etc/shells
+    info "adding $zsh_path to /etc/shells"
+    info "$zsh_path" | sudo tee -a /etc/shells
 fi
 
 if [[ "$SHELL" != "$zsh_path" ]]; then
     chsh -s "$zsh_path"
-    echo -e "${green}\\n\\ndefault shell changed to $zsh_path${reset}"
+    info "default shell changed to $zsh_path"
 fi
 
-echo "${green}creating vim directories${reset}"
+info "creating vim directories"
 mkdir -p ~/.vim-tmp
 
 if ! command_exists zsh; then
-    echo "${green}zsh not found. Please install and then re-run installation scripts${reset}"
+    info "zsh not found. Please install and then re-run installation scripts"
     exit 1
 elif ! [[ $SHELL =~ .*zsh.* ]]; then
-    echo "${greem}Configuring zsh as default shell${reset}"
+    info "Configuring zsh as default shell"
     chsh -s "$(command -v zsh)"
 fi
 
 # Change the default shell to zsh
 zsh_path="$( command -v zsh )"
 if ! grep "$zsh_path" /etc/shells; then
-    echo "${green}adding $zsh_path to /etc/shells${reset}"
-    echo "${green}$zsh_path${reset}" | sudo tee -a /etc/shells
+    info "adding $zsh_path to /etc/shells"
+    info "$zsh_path" | sudo tee -a /etc/shells
 fi
 
 if [[ "$SHELL" != "$zsh_path" ]]; then
     chsh -s "$zsh_path"
-    echo "${green}default shell changed to $zsh_path${reset}"
+    info "default shell changed to $zsh_path"
 fi
 
-echo "${green}Done. Reload your terminal.${reset}"
+info "Done. Reload your terminal."
