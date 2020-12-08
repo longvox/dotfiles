47dc19c2497b71dec8ee3251f13b1e53b9b5b850
Long Vo <longvox98@gmail.com>
2020-12-09 03:07:29 +0700

    [imlong] - install bash

---
+++ install/utils/install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source $(dirname "$(readlink -f "$BASH_SOURCE")")/log.sh
+
+isInstall() {
+    dpkg-query -Wf'${db:Status-abbrev}' "$1" 2>/dev/null | grep -q '^i'
+}
+
+install() {
+    for package in "$@"
+    do
+        isInstall $package && silly "$package installed, moving on..." || sudo apt-get install -y $package
+    done
+}
+
+installPip() {
+    for package in "$@"
+    do
+        pip3 list | grep $package > /dev/null && silly "$package installed, moving on..." || pip3 install $package
+    done
+}
+
+installDeb() {
+    URL=$1
+    BASE=$(basename ${URL})
+    FILE=$(printf '%b' ${BASE//%/\\x})
+    SOURCE_FILE="/home/$USER/.dotfiles/.temp-install/$FILE"
+    DOWNLOAD_FOLDER="/home/$USER/.dotfiles/.temp-install"
+    if [ ! -f "$SOURCE_FILE" ]; then 
+        wget -P $DOWNLOAD_FOLDER $URL
+    fi
+    sudo dpkg -i $SOURCE_FILE
+}
+
+installSh() {
+    URL=$1
+    BASE=$(basename ${URL})
+    FILE=$(printf '%b' ${BASE//%/\\x})
+    SOURCE_FILE="/home/$USER/.dotfiles/.temp-install/$FILE"
+    DOWNLOAD_FOLDER="/home/$USER/.dotfiles/.temp-install"
+    if [ ! -f "$SOURCE_FILE" ]; then 
+        wget -P $DOWNLOAD_FOLDER $URL
+    fi
+    bash $SOURCE_FILE
+}
